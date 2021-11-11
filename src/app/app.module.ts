a2fbd3837f8b7964075d837a3545e34717d5e97e
emarion17 <eliane.rosa@etec.sp.gov.br>
2021-11-11 09:00:20 -0300

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CadastroPacoteComponent } from './cadastro-pacote/cadastro-pacote.component';
+import { DetalhesPacoteComponent } from './detalhes-pacote/detalhes-pacote.component';
+import { ExclusaoPacoteComponent } from './exclusao-pacote/exclusao-pacote.component';
+import { ListagemPacoteComponent } from './listagem-pacote/listagem-pacote.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { AprovacaoPacoteComponent } from './aprovacao-pacote/aprovacao-pacote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroPacoteComponent,
+    DetalhesPacoteComponent,
+    ExclusaoPacoteComponent,
+    ListagemPacoteComponent,
+    HeaderComponent,
+    HomeComponent,
+    AprovacaoPacoteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
